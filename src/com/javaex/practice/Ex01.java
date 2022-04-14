5ca9a458bf6119528ed01f357b271dfd54886d70
y <y@DESKTOP-72F38EA>
2022-04-14 18:23:56 +0900

    2022-04-14

---
+++ src/com/javaex/practice/Ex01.java
@@ -0,0 +1,10 @@
+package com.javaex.practice;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = 1;
+		
+		int a = ++x *2;
+		int b = y++ *2;
+		
+		System.out.println("a=" + a); // (++1)*2 → 2*2 → 4 
+		System.out.println("b=" + b); // (1++)*2 → 1*2 → 2출력, 후 2*2 → 4
+		System.out.println("x=" + x); // 2
+		System.out.println("y=" + y); // 2
+		
+	}
+	
+}
